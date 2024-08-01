70a7195dce148989ae9c1e2f359babb048a7d57f
Harjot Singh <HarjjotSinghh@gmail.com>
2024-08-01 09:32:54 +0530

    remove: unused vars

--- components/home-page.tsx
+++ components/home-page.tsx
@@ -433,25 +433,6 @@ export default function HomePage() {
   );
 }
 
-function FacebookIcon(props: ComponentPropsWithoutRef<"svg">) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
-    </svg>
-  );
-}
-
 function InstagramIcon(props: ComponentPropsWithoutRef<"svg">) {
   return (
     <svg
@@ -469,27 +450,6 @@ function InstagramIcon(props: ComponentPropsWithoutRef<"svg">) {
       <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
       <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
       <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" />
-    </svg>
-  );
-}
-
-function TextIcon(props: ComponentPropsWithoutRef<"svg">) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M17 6.1H3" />
-      <path d="M21 12.1H3" />
-      <path d="M15.1 18H3" />
     </svg>
   );
 }
